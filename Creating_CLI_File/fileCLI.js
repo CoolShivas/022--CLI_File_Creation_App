c93ea2caaecc9b334bd2337e2df97aa54bb64c65
CoolShivas <shivas2710cool00@gmail.com>
2025-03-16 18:33:44 +0530

    readline.createInterface for read and write data.

---
+++ Creating_CLI_File/fileCLI.js
@@ -0,0 +1,8 @@
+import readline from "readline";
+
+// // // // readline.createInterface for read and write data;
+const lineReader = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
