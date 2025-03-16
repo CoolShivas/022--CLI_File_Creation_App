8142d4f07d41537f7046f2f44866f052fc4a4af3
CoolShivas <shivas2710cool00@gmail.com>
2025-03-16 19:07:20 +0530

    Created the function named as fileCreation.

--- Creating_CLI_File/fileCLI.js
+++ Creating_CLI_File/fileCLI.js
@@ -6,3 +6,14 @@ const lineReader = readline.createInterface({
     output : process.stdout
 });
 
+const fileCreation = () => {
+    console.log("\nEnter your file name : ");
+    console.log("Enter the content of your file : ");
+    lineReader.close(); // // Closing the interface now;
+};
+
+fileCreation();
+// // // // Here, we are getting the Output on Terminal as :-
+// // // // Enter your file name : 
+// // // // Enter the content of your file : 
+// // // // Completed running 'fileCLI.js'
